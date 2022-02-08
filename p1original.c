f4e58ad65b9217206148a89edaa5c1dd3718b023
mani12kanta <94883311+mani12robot@users.noreply.github.com>
2022-02-08 04:31:40 +0000

    done

--- p1original.c
+++ p1original.c
@@ -1,2 +1,24 @@
+#include<stdio.h>
+void input(float *base,float *height)
+{
+  printf("enter the base and height\n");
+  scanf("%f%f",base,height);
+}
+void find_area(float base,float hight,float *area)
+{
+  *area=((base*hight)/2);
+}
+void output(float base ,float height ,float area)
+{
+  printf("area of %f base and %f hight is %f\n",base ,height,area);
+}
+int main()
+{
+  float  base ,height ,area ;
+  input(&base,&height);
+  find_area(base,height ,&area);
+  output(base ,height ,area);
+  return 0;
 
 }
+
